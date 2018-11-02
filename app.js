96529d3342e769006ca156ca46bd7b714e0bc614
FendyPierre <fendy_tester@hotmail.com>
2018-11-01 19:57:39 -0500

    setup app

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require("express");
+app = express();
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+    res.render("landing")
+});
+
+app.listen(3000, function () {
+    console.log("Server has started!");
+});
