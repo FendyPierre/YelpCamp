3e0d30d6280ce8aafb0a503dce374edb3cd775ef
FendyPierre <fendy_tester@hotmail.com>
2018-11-02 18:15:11 -0500

    setup and implement mongoose

--- app.js
+++ app.js
@@ -1,9 +1,39 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+var express     = require("express");
+    app         = express(),
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose");
+
+mongoose.connect("mongodb://localhost/yelp_camp");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+//schema setup
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+function createCamp(name, image){
+    Campground.create(
+        {
+
+        name: name, 
+        image: image
+
+        }, function(err, camp){
+            if(err){
+                console.log("failed to create");
+                console.log(err);
+            }
+            else{
+                console.log("successfully created");
+                console.log(camp);
+            }
+    });
+}
 
 var campgrounds = [
     {name: "salmon Creek", image: "https://images.pexels.com/photos/33109/fall-autumn-red-season.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"},
@@ -24,7 +54,16 @@ app.get("/", function(req, res){
 });
 
 app.get("/campgrounds", function(req,res){
-    res.render("campgrounds", {campgrounds: campgrounds});
+    var campgrounds = Campground.find({}, function (err, allCamps) { 
+        if(err){
+            console.log("failed to create");
+            console.log(err);
+        }
+        else{
+            res.render("campgrounds", {campgrounds: allCamps});
+        }
+     })
+    
 });
 
 app.get("/campgrounds/new", function(req, res){
@@ -38,8 +77,7 @@ app.post("/campgrounds", function(req,res){
     var image = req.body.image;
 
     //add data to campgrounds
-    var newcamp = {name: name, image: image}
-    campgrounds.push(newcamp);
+    createCamp(name, image);
 
     //redirect to campgrounds page
     res.redirect("campgrounds");
